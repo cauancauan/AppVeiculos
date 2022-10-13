701d8ca229a3837a071298d7458fefc28b7474c8
CAUAN SANTANA SILVA <cauan.ssilva@senacsp.edu.br>
2022-10-13 17:10:51 -0300

    Enviar para o banco de dados

---
+++ app/Http/Controllers/CaminhaoController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Caminhao;
+use Illuminate\Support\Facades\Redirect;
+
+class CaminhaoController extends Controller
+{
+    public function FormularioCadastro(){
+        return view('cadastrarCaminhao');
+    }
+
+    public function Editar(){
+        return view('editarCaminhao');
+    }
+
+    public function SalvarBanco(Request $request){
+        $dadosCaminhao = $request->validate([
+            'modelo' => 'string|required',
+            'marca' => 'string|required',
+            'ano' => 'string|required',
+            'cor' => 'string|required',
+            'valor' =>'string|nullable'
+        ]);
+
+        Caminhao::create($dadosCaminhao);
+
+        return Redirect::route('home');
+    }
+
+
+        
+    
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('padrao')
+@section('content')
+<section class="img">
+    <img src="{{'/img/telaprincipal.gif'}}" />
+</section>
+@endsection
